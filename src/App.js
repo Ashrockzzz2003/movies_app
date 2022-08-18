fb84d4719c95fda9d588e1709f90de310c65b0b3
Ashwin Narayanan S <ashrockzzz2003@gmail.com>
2022-08-18 13:02:12 +0530

    Updated App.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from "react";
+import { useEffect,useState } from "react";
+import './App.css'
+import SearchIcon from './search.svg'
+import Movie from "./Movie";
+
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=4bc1df67"
+
+const App = () => {
+
+
+  const [movies,setMovies] = useState([]);
+  const [searchTerm,setSearchTerm] = useState([]);
+
+  const serachMovies = async (title) =>{
+
+    const result = await fetch(`${API_URL}&s=${title}`);  
+    const data = await result.json();
+    setMovies(data.Search);
+
+
+  }
+
+  useEffect( () => {
+    serachMovies('Avengers')
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <form action="" id="search_recipes">
+                <input placeholder="Search For Movie" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+                <i class="fa fa-search" onClick={ ()=> serachMovies(searchTerm) }></i>
+      </form>
+
+      {
+        movies?.length > 0
+        
+        ?( <div className="container">
+            { movies.map((movie) => {
+               return  <Movie movie={movie} />         
+            })
+          } 
+          </div>
+        ) :(
+           <div className="empty">
+             <h2>No movie found</h2>
+             </div>
+        )
+      }
+      
+      
     </div>
   );
 }
